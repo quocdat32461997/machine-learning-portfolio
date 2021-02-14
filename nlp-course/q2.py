196e07c4d9e6727428be6aa77da07bf64f799c6b
Eric Ngo <quocdat32461997@yahoo.com>
2021-02-13 22:05:23 -0600

    Added SSN tests

---
+++ nlp-course/q2.py
@@ -0,0 +1,18 @@
+"""
+Author: Dat Quoc Ngo
+Last modified: Feb. 13, 2021
+"""
+
+# import dependencies
+import argparse
+
+def main(args):
+	
+
+if __name__ == '__main__':
+	# initialize argument-parser object
+	paraser = argparse.ArgumentParser()
+
+	# add arguments
+	parser.add_argument('--file')
+	main(parser.parse_args())

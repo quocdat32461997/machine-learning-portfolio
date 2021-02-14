f33f6030157c37d83d82e414b2222fb7079e4dd6
Eric Ngo <quocdat32461997@yahoo.com>
2021-02-13 23:17:41 -0600

    Added phone-number regex and tests

---
+++ nlp-course/q3.py
@@ -0,0 +1,33 @@
+"""
+Author: Dat Quoc Ngo
+Last modified: Feb. 13, 2021
+"""
+
+# import dependencies
+import argparse
+import re
+
+def main(args):
+	# read file
+	with open(args.file, 'r') as file:
+		text = file.read().split('\n')
+
+	# regex matcher
+	pattern = r'\+((([1-9]\d)|(\d[1-9]))-(([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))-(([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))-\b(([1-9]\d{3})|(\d[1-9]\d{2})|(\d{2}[1-9]\d)|(\d{3}[1-9]))\b)|(\((([1-9]\d)|(\d[1-9]))\)-(([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))-(([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))-\b(([1-9]\d{3})|(\d[1-9]\d{2})|(\d{2}[1-9]\d)|(\d{3}[1-9]))\b)|(\((([1-9]\d)|(\d[1-9]))\)-\((([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))\)-\((([1-9]\d{2})|(\d[1-9]\d)|(\d{2}[1-9]))\)-\((([1-9]\d{3})|(\d[1-9]\d{2})|(\d{2}[1-9]\d)|(\d{3}[1-9]))\))'
+
+	# print matches
+	for txt in text:
+		# search ssn
+		match = re.search(pattern, txt)
+
+		# if a match, print out
+		if match:
+			print("Find {}".format(match.string))
+
+if __name__ == '__main__':
+	# initialize argument-parser object
+	parser = argparse.ArgumentParser()
+
+	# add arguments
+	parser.add_argument('--file', help = 'Path to SSN file', default = 'pn.txt')
+	main(parser.parse_args())

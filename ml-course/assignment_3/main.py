625e2f7a55eb160d726d2cd8200073f88f8258ea
Eric Ngo <quocdat32461997@yahoo.com>
2021-03-16 10:04:19 -0500

    update learning materials

---
+++ ml-course/assignment_3/main.py
@@ -0,0 +1,23 @@
+"""
+Author: Dat Quoc Ngo
+NET-ID: dqn170000
+File: name.py
+"""
+
+# import dependencies
+import os
+import argparse
+
+def main(args):
+
+	return None
+
+if __name__ == '__main__':
+	# initialize arg-parser
+	parser = argparse.ArgumentParser('Argument Parser')
+
+	# add arguments
+	parser.add_argument()
+
+	# execute
+	main(parser.parse_args())

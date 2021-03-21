b935fa2e4f2d53f0cad4d675dce26a131c0e1e41
Eric Ngo <quocdat32461997@yahoo.com>
2021-03-20 21:03:07 -0500

    Added complete training pipeline

--- gcp/custom_ml_gcp/trainer/model.py
+++ gcp/custom_ml_gcp/trainer/model.py
@@ -3,43 +3,48 @@ import tensorflow as tf
 from tensorflow.keras.layers import Flatten, Dense
 
 def input_fn(path, height, width, batch_size, seed = 2021):
-	"""
-	Load data, map transformations, and convert to tf.data API
-	Args:
-		path : path to images
-		height : desired image height
-		width : desired image width
-		batch_size : number of samples per batch
-		seed : random seed
-	Returns:
-		dataset : tf.data.Dataset instance
-	"""
+    """
+    Load data, map transformations, and convert to tf.data API
+    Args:
+        path : path to images
+        height : desired image height
+        width : desired image width
+        batch_size : number of samples per batch
+        seed : random seed
+    Returns:
+        dataset : tf.data.Dataset instance
+    """
 
-	# convert images to tf.data.Datast objects
-	train_dataset = tf.keras.preprocessing.image_dataset_from_directory(path,
-		validation_split = 0.2,
-		subset = 'training',
-		seed = seed,
-		image_size = (height, width),
-		batch_size = batch_size)
+    # convert images to tf.data.Datast objects
+    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(path,
+        validation_split = 0.2,
+	subset = 'training',
+	seed = seed,
+	image_size = (height, width),
+	batch_size = batch_size)
 
-	val_dataset = tf.keras.preprocessing.image_dataset_from_directory(path,
-		validation_split = 0.2,
-		subset = 'validation',
-		seed = seed,
-		image_size = (height, width),
-		batch_size = batch_size)
+    val_dataset = tf.keras.preprocessing.image_dataset_from_directory(path,
+	validation_split = 0.2,
+	subset = 'validation',
+	seed = seed,
+	image_size = (height, width),
+	batch_size = batch_size)
 
-	#  map transformations
-	train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size = tf.data.AUTOTUNE)
-	val_dataset = val_dataset.cache().prefetch(buffer_size = tf.data.AUTOTUNE)
+    #  map transformations
+    train_dataset = train_dataset.cache().shuffle(1000)
+    val_dataset = val_dataset.cache()
 
-	# preprocessing (this step needs to handled separately when in prediction)
-	normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-	train_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
-	val_datast = val_dataset.map(lambda x, y: (normalization_layer(x), y))
+    # resize
+    resize_method = lambda img, labels: (tf.image.resize_with_pad(img, height, width, method = tf.image.ResizeMethod.BILINEAR), labels)
+    train_dataset = train_dataset.map(resize_method)
+    val_dataset = val_dataset.map(resize_method)
 
-	return train_dataset, val_dataset
+    # preprocessing (this step needs to handled separately when in prediction)
+    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    train_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
+    val_datast = val_dataset.map(lambda x, y: (normalization_layer(x), y))
+
+    return train_dataset.prefetch(buffer_size = tf.data.AUTOTUNE), val_dataset.prefetch(buffer_size = tf.data.AUTOTUNE)
 
 def create_model(num_class, shape):
     """
--- gcp/custom_ml_gcp/trainer/task.py
+++ gcp/custom_ml_gcp/trainer/task.py
@@ -9,65 +9,69 @@ from . import util
 from . import model
 
 def get_args():
-        """
-        Arugment parser
-        Inputs: _
-        Outputs:
-                args : dictionary of arguments
-        """
-        # initialize argument parser
-        parser = argparse.ArgumentParser()
+    """
+    Arugment parser
+    Inputs: _
+    Outputs:
+        args : dictionary of arguments
+    """
+    # initialize argument parser
+    parser = argparse.ArgumentParser()
 
-        # add arguments
-        parser.add_argument('--model-name', type = str,
-		help = 'Name of your mode', default = 'cnn')
-        parser.add_argument('--job-dir', type = str,
-                help = 'local or Google Cloud Storage location for writing checkpoints and exporting models',
-                default = 'checkpoints')
-        parser.add_argument('--num-epochs',
-                type = int, default = 20,
-                help = 'number of iterations to go through the data, default = 20')
-        parser.add_argument('--batch-size',
-                type = int, default = 128,
-                help = 'number of samples to read for each training step, default = 128')
-        parser.add_argument('--learning-rate',
-                type = float, default = 0.1,
-                help = 'learning rate for gradient descent, default = 0.1')
-        parser.add_argument('--verbosity',
-                choices = ['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
-                default = 'INFO')
-        parser.add_argument('--predict-num',
-                type = int, default = 1, help = 'Number of predictions. If 1, online prediction. If > 1, batch prediction')
+    # add arguments
+    parser.add_argument('--model-name', type = str,
+        help = 'Name of your mode', default = 'cnn')
+    parser.add_argument('--job-dir', type = str,
+        help = 'local or Google Cloud Storage location for writing checkpoints and exporting models',
+        default = 'checkpoints')
+    parser.add_argument('--num-epochs',
+        type = int, default = 20,
+        help = 'number of iterations to go through the data, default = 20')
+    parser.add_argument('--batch-size',
+        type = int, default = 32,
+        help = 'number of samples to read for each training step, default = 128')
+    parser.add_argument('--learning-rate',
+        type = float, default = 0.001,
+        help = 'learning rate for gradient descent, default = 0.1')
+    parser.add_argument('--verbosity',
+        choices = ['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+        default = 'INFO')
+    parser.add_argument('--predict-num',
+        type = int, default = 1, help = 'Number of predictions. If 1, online prediction. If > 1, batch prediction')
 
-        return parser.parse_args()
+    return parser.parse_args()
 
 def train_and_evaluate(args):
-        # load data
-        data_dir = util.load_data()
+    # height and width
+    height, width = 224, 224
 
-        # create train and validation datasets
-        train_dataset, val_dataset = model.input_fn(data_dir, height = 180, width = 180, batch_size = args.batch_size)
+    # load data
+    data_dir = util.load_data()
 
-        for x in train_dataset.take(1):
-            print(x[0].shape, x[1].shape)
+    # create train and validation datasets
+    train_dataset, val_dataset = model.input_fn(data_dir, height = height, width = width, batch_size = args.batch_size)
 
-	# create model
-        network = model.create_model(num_class = 5, shape = (224, 224, 3))
-        print(network.summary())
+    for x in train_dataset.take(1):
+        print(x[0].shape, x[1])
 
-	# setup hyperparameters
-        loss = tf.keras.losses.CategoricalCrossentropy()
-        optimizer = tf.keras.optimizers.Adam(learning_rate = args.learning_rate)
-        callbacks = []
+    # create model
+    network = model.create_model(num_class = 5, shape = (height, width, 3))
+    print(network.summary())
 
-        network.compile(loss = loss, optimizer = optimizer, metrics = [
-            tf.keras.metrics.AUC(multi_label = False, thresholds = [0.5]),
-            tf.keras.metrics.Precision(),
-            tf.keras.metrics.Recall()])
+    # setup hyperparameters
+    loss = tf.keras.losses.SparseCategoricalCrossentropy()
+    optimizer = tf.keras.optimizers.Adam(learning_rate = args.learning_rate)
+    callbacks = []
 
-	# train model
-        #network.fit(train_dataset, validation_data = val_dataset, epochs = args.num_epochs, batch_size = args.batch_size, verbose = 1, callbacks = callbacks)
-	# export model
-        #export_path = os.path.join(args.job_dir, args.model_name, str(np.floor(datetime.utcnow().timestamp())))
-        #tf.keras.models.save_model(network, export_path)
-        return None
+    network.compile(loss = loss, optimizer = optimizer, metrics = ['accuracy'])
+
+    # train model
+    network.fit(train_dataset, validation_data = val_dataset, epochs = args.num_epochs, batch_size = args.batch_size, verbose = 1, callbacks = callbacks)
+    # export model
+    export_path = os.path.join(args.job_dir, args.model_name, str(np.floor(datetime.utcnow().timestamp())))
+    tf.keras.models.save_model(network, export_path)
+    return None
+
+if __name__ == '__main__':
+    args = get_args()
+    train_and_evaluate(args)
